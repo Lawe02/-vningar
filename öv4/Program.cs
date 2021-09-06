ae7b5d92d22441637639631cf9a3e32500f0122d
Lawe Zangena <lawza02@gmail.com>
2021-09-06 14:58:39 +0200

    Lagt till ignore fil

---
+++ öv4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace öv4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ öv5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace öv5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
